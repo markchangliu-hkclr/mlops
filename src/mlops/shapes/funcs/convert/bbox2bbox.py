c8852eb6ec6546fac56d735523f13294fe9c9f39
Liu Chang <cliu@hkclr.hk>
2026-01-08 15:51:41 +0800

    update

--- src/mlops/shapes/funcs/convert/bbox2bbox.py
+++ src/mlops/shapes/funcs/convert/bbox2bbox.py
@@ -1,14 +1,26 @@
+from typing import Tuple
+
+import numpy as np
+
 from ...typing import (
     BBoxesArrXYWHType,
     BBoxesArrXYXYType,
-    BBoxesLabelmeType
+    BBoxesLabelmeType,
+    BBoxesLabelmeType,
+    BBoxesCocoType,
+    BBoxesYoloType,
 )
 
 
 __all__ = [
     "bboxesArrXYXY_to_bboxesArrXYWH",
     "bboxesArrXYWH_to_bboxesArrXYXY",
-    "bboxesArrXYXY_to_bboxesLabelme"
+    "bboxesArrXYXY_to_bboxesLabelme",
+    "bboxesArrXYXY_to_bboxesCoco",
+    "bboxesArrXYXY_to_bboxesYolo",
+    "bboxesLabelme_to_bboxesArrXYXY",
+    "bboxesCoco_to_bboxesArrXYXY",
+    "bboxesYolo_to_bboxesArrXYXY"
 ]
 
 
@@ -31,3 +43,50 @@ def bboxesArrXYXY_to_bboxesLabelme(
 ) -> BBoxesLabelmeType:
     bboxes_labelme = bboxes_xyxy.copy().reshape(-1, 2, 2).tolist()
     return bboxes_labelme
+
+def bboxesArrXYXY_to_bboxesCoco(
+    bboxes: BBoxesArrXYXYType
+) -> BBoxesCocoType:
+    bboxes = bboxes.copy()
+    bboxes[:, [2, 3]] = bboxes[:, [2, 3]] - bboxes[:, [0, 1]]
+    bboxes = bboxes.tolist()
+    return bboxes
+
+def bboxesArrXYXY_to_bboxesYolo(
+    bboxes: BBoxesArrXYXYType,
+    img_hw: Tuple[int, int]
+) -> BBoxesYoloType:
+    img_h, img_w = img_hw
+    bboxes = bboxes.copy()
+    bboxes[:, [2, 3]] = bboxes[:, [2, 3]] - bboxes[:, [0, 1]]
+    bboxes[:, [0, 1]] = bboxes[:, [0, 1]] + 0.5 * bboxes[:, [2, 3]]
+    bboxes[:, [0, 2]] = bboxes[:, [0, 2]] / img_w
+    bboxes[:, [1, 3]] = bboxes[:, [1, 3]] / img_h
+    bboxes = bboxes.tolist()
+    return bboxes
+
+def bboxesLabelme_to_bboxesArrXYXY(
+    bboxes_labelme: BBoxesLabelmeType
+) -> BBoxesArrXYXYType:
+    bboxes_arr = np.asarray(bboxes_labelme).reshape(-1, 4)
+    return bboxes_arr
+
+def bboxesCoco_to_bboxesArrXYXY(
+    bboxes_coco: BBoxesCocoType
+) -> BBoxesArrXYXYType:
+    bboxes = np.asarray(bboxes_coco).astype(np.int32)
+    bboxes[:, [2, 3]] = bboxes[:, [0, 1]] + bboxes[:, [2, 3]]
+    return bboxes
+
+def bboxesYolo_to_bboxesArrXYXY(
+    bboxes: BBoxesYoloType,
+    img_hw: Tuple[int, int]
+) -> BBoxesArrXYXYType:
+    img_h, img_w = img_hw
+    bboxes = np.asarray(bboxes)
+    bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * img_w
+    bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * img_h
+    bboxes[:, [0, 1]] = bboxes[:, [0, 1]] - 0.5 * bboxes[:, [2, 3]]
+    bboxes[:, [2, 3]] = bboxes[:, [0, 1]] + bboxes[:, [2, 3]]
+    bboxes = bboxes.astype(np.int32)
+    return bboxes
